648e8fe1738d444231f85de57c17e08221cd9c5b
Devsam_ <sabillyamri1@gmail.com>
2025-06-30 08:21:59 +0700

    cek

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        primary: ['Poppins', 'sans-serif'],
-        script: ['Oleo Script', 'cursive'],
+        primary: ["Poppins", "sans-serif"],
+        script: ["Oleo Script", "cursive"],
       },
       colors: {
-        primary: '#00A8CD',
-        background: '#14181A',
+        primary: "#00A8CD",
+        background: "#14181A",
       },
       animation: {
-        'spin-slow': 'spin 20s linear infinite',
-        'spin-slow-reverse': 'spin 15s linear infinite reverse',
+        "spin-slow": "spin 20s linear infinite",
+        "spin-slow-reverse": "spin 15s linear infinite reverse",
       },
     },
   },
   plugins: [],
-}
+};
